c883776a72f84ac249afe51bb8835d3b3a20817f
vedant koditkar <vedant.koditkar.vk@gmail.com>
2017-03-11 18:37:03 +0530

    Added Link component provided by react-router to create link button to navigate from posts to create post route

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -3,6 +3,8 @@ import React from 'react'
 //used to connect component to redux state
 import { connect } from 'react-redux'
 
+import { Link } from 'react-router'
+
 //action creator
 import { fetchPosts } from '../actions/index'
 
@@ -16,6 +18,13 @@ class PostsIndex extends React.Component {
     render() {
         return (
             <div>
+                <div className="text-xs-right">
+                    <Link
+                        to="posts/new"
+                        className="btn btn-sm btn-primary">
+                        {'Add a Post'}
+                    </Link>
+                </div>
                 <div>List of Posts</div>
             </div>
         )
