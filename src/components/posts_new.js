78516c9254bb27bdfa8d8285c91708718a213c23
vedant koditkar <vedant.koditkar.vk@gmail.com>
2017-03-12 13:02:20 +0530

    Added redux form, action creator to create post

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,11 +1,45 @@
 import React from 'react'
+import { reduxForm } from 'redux-form'
+import { createPost } from '../actions/index'
 
 class PostsNew extends React.Component {
     render() {
+        const {
+            handleSubmit,
+            fields: {
+                title,
+                categories,
+                content
+            }
+        } = this.props
+
         return (
-            <div>Create Blob</div>
+            <form onSubmit={handleSubmit(this.props.createPost)}>
+                <h3>Create A New Post</h3>
+                <div className="form-group">
+                    <label>{'Title'}</label>
+                    <input type="text" className="form-control" {...title}/>
+                </div>
+                <div className="form-group">
+                    <label>{'Catrgories'}</label>
+                    <input type="text" className="form-control" {...categories}/>
+                </div>
+                <div className="form-group">
+                    <label>{'Content'}</label>
+                    <textarea className="form-control" {...content}/>
+                </div>
+
+                <button
+                    type="submit"
+                    className="btn btn-sm btn-primary">
+                    {'Submit'}
+                </button>
+            </form>
         )
     }
 }
 
-export default PostsNew
+export default reduxForm({
+    form: 'PostNewForm',
+    fields: ['title','categories','content']
+}, null, { createPost })(PostsNew)
